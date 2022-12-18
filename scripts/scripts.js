91f844531de237f09d5e01eb3dc6eba14f5d5893
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-18 18:06:59 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,17 @@
-function capatalize(strg) {
-  let loweString = strg.toLowerCase();
-  let higerString = loweString.charAT(0).to;
-  return loweString;
+"use strict";
+
+let answer = parseInt(
+  prompt("Please enter the number you would like to FizzBuzz up to: ")
+);
+
+for (let i = 0; i < answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
-console.log(capatalize("HELLO MY NAME IS Paul"));
