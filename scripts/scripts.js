540c3236742a8f0fc040ea95dc353f690e477e3e
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-19 21:29:28 -0500

    Nothing to update
    Just used to work on some exercises

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,2 @@
 "use strict";
 
-let answer = parseInt(
-  prompt("Please enter the number you would like to FizzBuzz up to: ")
-);
-
-for (let i = 0; i < answer; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
